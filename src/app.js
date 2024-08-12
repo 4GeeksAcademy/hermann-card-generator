2241c05acfec189c6923608af31679c6e9e78c31
hermannramos <140882889+hermannramos@users.noreply.github.com>
2024-08-12 16:54:46 +0000

    trabajo finalizado

--- src/app.js
+++ src/app.js
@@ -7,8 +7,16 @@ import "./assets/img/4geeks.ico";
 
 let icons = ["♦", "♥", "♠", "♣"];
 
-function generateCard() {
-  let numbers = Math.floor(Math.random() * 10) + 1;
+window.onload = function generateCard() {
+  let num = Math.floor(Math.random() * 10) + 1;
   let icon = icons[Math.floor(Math.random() * icons.length)];
   let carta = document.getElementById("carta");
+
+  let color = (icon === "♦" || icon === "♥") ? "red" : "black";
+
+  carta.innerHTML = `
+  <div class="icon-right" style ="color: ${color}">${icon}</div>
+  <div class="num">${num}</div>
+  <div class="icon-left" style ="color: ${color}">${icon}</div>
+  `;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -73,8 +73,5 @@ module.exports = {
         filename: "index.html",
         template: "src/index.html"
     }),
-    new PrettierPlugin({
-        failSilently: true
-    }),
   ]
 };
